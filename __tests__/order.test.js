77dd61f77465d5c3927f9367d9da65b47582f586
Antonio Pierro <antonio.pierro@gmail.com>
2022-04-07 13:22:02 +0200

    style: change workflow file name

---
+++ __tests__/order.test.js
@@ -0,0 +1,15 @@
+const myBeverage = {
+    delicious: true,
+    sour: false,
+};
+
+describe('my beverage', () => {
+    test('is delicious', () => {
+        expect(myBeverage.delicious).toBeTruthy();
+    });
+
+    test('is not sour', () => {
+        expect(myBeverage.sour).toBeFalsy();
+    });
+});
+
